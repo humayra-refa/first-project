8f638943e879ee0075f2b2c5b352a78f3e1d5f5e
Iffat Humayra Refa <humayra.refa@gmail.com>
2026-03-10 10:26:07 +0600

    starting

---
+++ module1/printing_file.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int age;
+    char initial;
+    cout<<"Enter your age :";
+    cin>>age;
+    cout<<age<<endl;
+    cout<<"Enter a letter :";
+    cin>>initial;
+    cout<<initial<<endl;
+    return 0;
+}
